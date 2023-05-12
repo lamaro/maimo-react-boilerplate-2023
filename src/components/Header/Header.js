366a2c0fbb1b0cf008f496bd624338c01374d366
Leandro Amaro Garcia <leanamaro@gmail.com>
2023-05-11 21:45:00 -0300

    add wrapper

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './Header.module.css';
 
-const Header = () => {
+const Header = ({title}) => {
   return <header className={styles.header_container}>
     <h1>
       <img src="/assets/logo512.png" alt="Logo" />
+      <span>{title}</span>
     </h1>
     <nav>
       <ul>
