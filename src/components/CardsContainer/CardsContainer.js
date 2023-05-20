09e6894ce6f1d6662e104758b22d03d5104e8478
Agustin <agustinborsaumai@gmail.com>
2023-05-19 21:19:57 -0300

    a lot of changes regarding to cards logic

--- src/components/CardsContainer/CardsContainer.js
+++ src/components/CardsContainer/CardsContainer.js
@@ -2,14 +2,44 @@ import React from "react";
 import Card from "../Card/Card";
 import Styles from "./CardsContainer.module.css"
 
+const data = [
+  {
+    name: 'Colombia',
+    image: {
+      medium:
+        'https://static.tvmaze.com/uploads/images/medium_portrait/6/16463.jpg',
+      original:
+        'https://static.tvmaze.com/uploads/images/original_untouched/6/16463.jpg',
+    },
+    description: "Esta región, ubicada al suroeste de Colombia en el Nudo de los Pastos, se caracteriza por su diversidad topográfica y su riqueza de vertientes."
+  },
+  {
+    name: 'Brasil',
+    image: {
+      medium:
+        'https://static.tvmaze.com/uploads/images/medium_portrait/38/96515.jpg',
+      original:
+        'https://static.tvmaze.com/uploads/images/original_untouched/38/96515.jpg',
+    },
+    description: "Proviene de los departamentos de Caquetá y Putumayo, ubicados en la región sur oriental de Colombia, específicamente donde se observa el pie de monte amazónico."
+  },
+  {
+    name: 'Expreso',
+    image: {
+      medium: "https://static.tvmaze.com/uploads/images/medium_portrait/295/738675.jpg",
+      original: "https://static.tvmaze.com/uploads/images/original_untouched/295/738675.jpg"
+    },
+    description: "Se caracteriza por su rápida preparación a una alta presión y por un sabor y textura más concentrados."
+  },
+];
+
 const CardsContainer = () => {
   return (
-    <ul className={Styles.cardsContainer}>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </ul>
+    <section className={Styles.cardsContainer}>
+       {data.map((cafe) => (
+        <Card name={cafe.name} image={cafe.image.original} description={cafe.description} />
+      ))}
+    </section>
   );
 };
 
