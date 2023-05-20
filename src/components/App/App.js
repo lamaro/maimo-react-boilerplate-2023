c60a8c4e978b071d774eaac925cf034fbdcc865d
Leandro Amaro Garcia <leanamaro@gmail.com>
2023-05-19 21:32:38 -0300

    added cards

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,14 +3,19 @@ import Header from '../Header/Header';
 import Hero from '../Hero/Hero';
 import Footer from '../Footer/Footer';
 import CardsContainer from '../CardsContainer/CardsContainer';
+import { Element } from 'react-scroll';
 
 const App = () => {
   return (
     <div className='App'>
-      <Header title={`mi titulo`} />
-      <Hero />
+      <Header title={`mi titulo 2`} />
+      <Element name='hero' className='element'>
+        <Hero />
+      </Element>
       <CardsContainer />
-      <Footer />
+      <Element name='footer' className='element'>
+        <Footer />
+      </Element>
     </div>
   );
 };
