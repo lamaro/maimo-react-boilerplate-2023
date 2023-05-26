2181ebca353913fac33f653db8a8f2cf699c13eb
Agustin <agustinborsaumai@gmail.com>
2023-05-26 11:48:22 -0300

    hero and header changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,13 +3,11 @@ import styles from './Header.module.css';
 
 const Header = () => {
   return <header className={styles.header_container}>
-    <h1>
-      <img src="/assets/logo512.png" alt="Logo" />
-    </h1>
     <nav>
       <ul>
         <li>Home</li>
         <li>Contact</li>
+        <li>Contact2</li>
       </ul>
     </nav>
   </header>;
