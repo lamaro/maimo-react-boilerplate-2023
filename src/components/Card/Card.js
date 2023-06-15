955d78cd28c3e6fce8235c45ca6038aa59198d30
Leandro Amaro Garcia <leanamaro@gmail.com>
2023-06-15 20:40:35 -0300

    feat: add axios

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import styles from './Card.module.css'
+import React from 'react';
+import styles from './Card.module.css';
 
-const Card = ({name, image, language}) => {
+const Card = ({ index, name, image, language, featured, handleFeatured }) => {
+
   return (
-    <article className={`col_4 ${styles.card_wrapper}`}>
+    <article
+      className={`col_4 ${styles.card_wrapper} ${
+        index === featured ? styles.selected : ''
+      }`}
+    >
       <img src={image} alt={name} />
       <h2>{name}</h2>
-      <p>{language}</p>
+      <p>
+        {language} {index}
+      </p>
+      <button onClick={() => handleFeatured(index)}>Select</button>
     </article>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
--- src/components/CardsContainer/CardsContainer.js
+++ src/components/CardsContainer/CardsContainer.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from '../Card/Card';
 import styles from './CardsContainer.module.css';
-import data from '../../utils/dummyData'
 
-const CardsContainer = () => {
+const CardsContainer = ({ data }) => {
+  const [featured, setFeatured] = useState(2);
+
+  const handleFeaturedSelect = (cardId) => {
+    console.log('SELECTED', cardId);
+    setFeatured(cardId);
+  };
+
   return (
     <section className={styles.cards_container}>
       <div className='grid'>
-        {data.map(({ name, image, language }) => (
-          <Card name={name} image={image.original} language={language} />
-        ))}
+        {data.map((movie, index) => {
+          const {name, image, language} = movie.show
+          return (
+            <Card
+              name={name}
+              image={image?.original}
+              language={language}
+              featured={featured}
+              handleFeatured={handleFeaturedSelect}
+              index={index}
+            />
+          );
+        })}
       </div>
     </section>
   );
